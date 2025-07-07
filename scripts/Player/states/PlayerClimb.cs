9b84e9bed59e4e3cb3e962db6c27e8d13640085a
Felipe Ginklings <felipe.ginklings@gmail.com>
2025-07-07 08:45:19 -0300

    feat: Atualiza posição do jogador e ajusta lógica de estado ao sair das escadas

--- scripts/Player/states/PlayerClimb.cs
+++ scripts/Player/states/PlayerClimb.cs
@@ -26,8 +26,10 @@ public partial class PlayerClimb : PlayerState {
         }
 
         if (player.IsOnFloor()) {
+            player.IsOnStairs = false; // Reset stairs state when on floor
             EmitSignal(State.SignalName.Transitioned, this, IDLE);
         } else if (Input.IsActionJustPressed(INPUT_JUMP)) {
+            player.IsOnStairs = false; // Reset stairs state when on floor
             EmitSignal(State.SignalName.Transitioned, this, JUMP);
         } else if (!player.IsOnStairs) {
             EmitSignal(State.SignalName.Transitioned, this, FALL);
